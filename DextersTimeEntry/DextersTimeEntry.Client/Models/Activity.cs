d19d2407dca33fac4dbb0ed7158fb6e0e7832fda
LUIS FELIPE LA ROTTA <larottao@gmail.com>
2025-08-29 15:01:21 -0600

    First commit

---
+++ DextersTimeEntry/DextersTimeEntry.Client/Models/Activity.cs
@@ -0,0 +1,15 @@
+﻿namespace DextersTimeEntry.Client.Models
+{
+    public class Activity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string ProjectCode { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+
+        public string AAType { get; set; } = "";
+        public List<DateTime> TimeSlots { get; set; } = new();
+    }
+
+}
---
+++ DextersTimeEntry/DextersTimeEntry/wwwroot/js/siteStorage.js
@@ -0,0 +1,30 @@
+﻿window.jsonStorage = {
+    saveFile: function (fileName, jsonData) {
+        const blob = new Blob([jsonData], { type: "application/json" });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        URL.revokeObjectURL(url);
+    },
+    loadFile: function () {
+        return new Promise((resolve, reject) => {
+            const input = document.createElement("input");
+            input.type = "file";
+            input.accept = ".json";
+            input.onchange = e => {
+                const file = e.target.files[0];
+                if (!file) {
+                    resolve(null);
+                    return;
+                }
+                const reader = new FileReader();
+                reader.onload = () => resolve(reader.result);
+                reader.onerror = () => reject(reader.error);
+                reader.readAsText(file);
+            };
+            input.click();
+        });
+    }
+};
